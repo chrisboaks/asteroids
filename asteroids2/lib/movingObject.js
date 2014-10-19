faba4bc629b0a50a09a5e528f5127a72ed7c8b8b
Christopher Boaks <chris.boaks@gmail.com>
2014-10-19 15:41:49 -0400

    Beef up Utils

---
+++ asteroids2/lib/movingObject.js
@@ -0,0 +1,49 @@
+(function () {
+  if (typeof Asteroids === 'undefined') {
+    window.Asteroids = {};
+  }
+
+  var MovingObject = Asteroids.MovingObject = function (options) {
+    this.pos = options.pos;
+    this.vel = options.vel;
+    this.color = options.color;
+    this.radius = options.radius;
+    this.game = options.game;
+  };
+
+  MovingObject.prototype.draw = function (ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(
+      this.pos[0],
+      this.pos[1],
+      this.radius,
+      0,
+      2 * Math.PI,
+      false);
+
+      ctx.fill();
+  };
+
+  MovingObject.prototype.isCollidedWith = function (otherObject) {
+    var dist = Asteroids.Util.distance(this.pos, otherObject.pos);
+    var maxDist = this.radius + otherObject.radius;
+    return (dist <= maxDist);
+  };
+
+
+
+  // incorrectly deals with non-vecs and wrapping
+  MovingObject.prototype.move = function () {
+    var possPos = this.game.wrap([this.posX += this.velX, this.posY += this.velY]);
+    if (this instanceof Asteroids.Bullet) {
+      this.posX += this.velX;
+      this.posY += this.velY;
+    } else {
+      this.posX = possPos[0];
+      this.posY = possPos[1];
+    }
+  };
+
+
+})();
---
+++ asteroids2/lib/util.js
@@ -0,0 +1,51 @@
+(function () {
+  if (typeof Asteroids === 'undefined') {
+    window.Asteroids = {};
+  }
+
+  var Util = Asteroids.Util = function () { }
+
+  var inherits = Asteroids.Util.inherits = function (descendant, baseClass) {
+    function Surrogate () {};
+    Surrogate.prototype = baseClass.prototype;
+    descendant.prototype = new Surrogate();
+  };
+
+  var randomVec = Asteroids.Util.randomVec = function (length) {
+    var randomTheta = Math.random() * 2 * Math.PI;
+    var velX = Math.cos(randomTheta) * length;
+    var velY = Math.sin(randomTheta) * length;
+    return [velX, velY];
+  };
+
+  var randomUnitVector = Asteroids.Util.randomUnitVector = function() {
+    var randomTheta = Math.random() * 2 * Math.PI;
+    var x = Math.cos(randomTheta);
+    var y = Math.sin(randomTheta);
+    return [x, y];
+  };
+
+  var randomVectorOfLength = Asteroids.Util.randomVectorOfLength = function(length) {
+    var unitVector = Util.randomUnitVector;
+    var x = unitVector[0];
+    var y = unitVector[1];
+    return [x * length, y * length];
+  };
+
+  var distance = Asteroids.Util.distance = function(vec1, vec2) {
+    dx = vec1[0] - vec2[0];
+    dy = vec1[1] - vec2[1];
+    return Math.sqrt(dx * dx + dy * dy);
+  };
+
+  var normalize = Asteroids.Util.normalize = function (vec) {
+    var thisLength = Utils.distance([0, 0], vec);
+    return [vec[0] / thisLength, vec[1] / thisLength];
+  }
+
+
+
+
+
+
+})();
